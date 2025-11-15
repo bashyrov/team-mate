5386669f69087a4dbda426aed6e90723b9202d55
Artur Bashyrov <bashyroov@gmail.com>
2025-11-15 23:33:39 +0100

    Replace auth urls path to users.urls

--- team_mate/urls.py
+++ team_mate/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
@@ -25,6 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls'), name='projects'),
     path('', include('users.urls'), name='users'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + debug_toolbar_urls()
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from users import views
 from projects import views as project_views
+from django.contrib.auth import views as auth_views
+
 
 
 app_name = 'users'
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/<int:user_pk>/', views.DeveloperDetailView.as_view(), name='profile'),
     path('leaderboard/', views.LeaderboardView.as_view(), name='leaderboard'),
     path('profile/<int:user_pk>/update/', views.DeveloperUpdateView.as_view(), name='profile_update'),
